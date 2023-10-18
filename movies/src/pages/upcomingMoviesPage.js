ce67cfdb724adc4ecc346052e38224bb3ea1fe17
Barry Nolan <76070900+Baz333@users.noreply.github.com>
2023-10-18 13:19:59 +0100

    Added upcoming movies page

---
+++ movies/src/pages/upcomingMoviesPage.js
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from "react";
+import {getUpcomingMovies} from "../api/tmdb-api";
+import PageTemplate from "../components/templateMovieListPage";
+
+const UpcomingMoviesPage = (props) => {
+    const [movies, setMovies] = useState([]);
+
+    const addToFavourites = (movieId) => {
+        const updatedMovies = movies.map((m) => 
+            m.id === movieId ? {...m, favourite: true} : m
+        );
+        setMovies(updatedMovies);
+    };
+
+    useEffect(() => {
+        getUpcomingMovies().then(movies => {
+            setMovies(movies);
+        });
+    }, []);
+    
+    return (
+        <PageTemplate
+            title="Upcoming Movies"
+            movies={movies}
+            selectFavourite={addToFavourites}
+        />
+    );
+};
+
+export default UpcomingMoviesPage;
