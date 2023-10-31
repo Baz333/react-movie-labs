b23e860f396d02fdc34674095bd7b46ef36f49df
Barry Nolan <76070900+Baz333@users.noreply.github.com>
2023-10-31 17:26:34 +0000

    Updating movie page to use react query

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -13,10 +13,19 @@ export const getMovies = () => {
     });
 };
   
-export const getMovie = id => {
+export const getMovie = (args) => {
+    const [, idPart] = args.queryKey;
+    const {id} = idPart;
     return fetch(
         `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
-    ).then(res => res.json());
+    ).then((response) => {
+        if(!response.ok) {
+            throw new Error(response.json().message);
+        }
+        return response.json();
+    }).catch((error) => {
+        throw error
+    });
 };
 
 export const getGenres = () => {
@@ -36,12 +45,21 @@ export const getGenres = () => {
     });
 };
 
-export const getMovieImages = (id) => {
+export const getMovieImages = ({queryKey}) => {
+    const [, idPart] = queryKey;
+    const {id} = idPart;
     return fetch(
         `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
     )
-    .then((res) => res.json())
-    .then((json) => json.posters);
+    .then((response) => {
+        if(!response.ok) {
+            throw new Error(response.json().message);
+        }
+        return response.json();
+    })
+    .catch((error) => {
+        throw error
+    });
 };
 
 export const getMovieReviews = (id) => {
