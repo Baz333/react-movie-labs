216914de25e16699edc471410387083968f4bf44
Barry Nolan <76070900+Baz333@users.noreply.github.com>
2023-11-07 12:21:44 +0000

    Fix upcoming movies page

--- movies/src/pages/upcomingMoviesPage.js
+++ movies/src/pages/upcomingMoviesPage.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react";
 import {getUpcomingMovies} from "../api/tmdb-api";
 import PageTemplate from "../components/templateMovieListPage";
+import AddToFavouritesIcon from "../components/cardIcons/addToFavourites";
 
 const UpcomingMoviesPage = (props) => {
     const [movies, setMovies] = useState([]);
@@ -22,7 +23,9 @@ const UpcomingMoviesPage = (props) => {
         <PageTemplate
             title="Upcoming Movies"
             movies={movies}
-            selectFavourite={addToFavourites}
+            action={(movie) => {
+                return <AddToFavouritesIcon movie={movie} />
+            }}
         />
     );
 };
