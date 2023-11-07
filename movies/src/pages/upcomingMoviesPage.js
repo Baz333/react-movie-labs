06e437f33387a4182e5a683201ca8922fbf950eb
Barry Nolan <76070900+Baz333@users.noreply.github.com>
2023-11-07 12:57:31 +0000

    Change upcoming movies page to use caching

--- movies/src/pages/upcomingMoviesPage.js
+++ movies/src/pages/upcomingMoviesPage.js
@@ -1,23 +1,23 @@
 import React, {useState, useEffect} from "react";
 import {getUpcomingMovies} from "../api/tmdb-api";
 import PageTemplate from "../components/templateMovieListPage";
+import {useQuery} from 'react-query';
+import Spinner from '../components/spinner';
 import AddToWatchlistIcon from "../components/cardIcons/addToWatchlist";
 
 const UpcomingMoviesPage = (props) => {
-    const [movies, setMovies] = useState([]);
+    const {data, error, isLoading, isError} = useQuery('upcoming', getUpcomingMovies)
 
-    const addToFavourites = (movieId) => {
-        const updatedMovies = movies.map((m) => 
-            m.id === movieId ? {...m, favourite: true} : m
-        );
-        setMovies(updatedMovies);
-    };
+    if(isLoading) {
+        return <Spinner />;
+    }
 
-    useEffect(() => {
-        getUpcomingMovies().then(movies => {
-            setMovies(movies);
-        });
-    }, []);
+    if(isError) {
+        return <h1>{error.message}</h1>;
+    }
+
+    const movies = data.results;
+    console.log("Movies: ", movies);
     
     return (
         <PageTemplate
